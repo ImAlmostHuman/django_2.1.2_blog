0649c74d5e9bcdb77f6665e1cebc619f38f22b23
Tom <thomas.ulman@gmail.com>
2019-02-19 19:51:28 +1100

    fixed timezone, added model for Post and set up with admin, tested migration with post

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-	{
-	'author': 'Uschi',
-	'title': 'I love Bones',
-	'content': 'Content about bones',
-	'date_posted': 'October 29, 2018'
-	},
-	{
-	'author': 'Uschi',
-	'title': 'More on Bones',
-	'content': 'Some more content about bones',
-	'date_posted': 'October 30, 2018'
-	},
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
