5ccd5e1878ab6300b5c4342a1f6bcd32899f715e
Tom <thomas.ulman@gmail.com>
2019-03-05 20:43:34 +1100

    added urls, detailed post view

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
+admin.site.site_header = 'Cave'
+admin.site.site_title = 'Cave'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('opensesame/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='Login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='Logout'),
     path('', include('blog.urls')),
+
 ]
